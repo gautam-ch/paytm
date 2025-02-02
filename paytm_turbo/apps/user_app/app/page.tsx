964f814856f3c66448293cd62e7ab9a064d04f51
gautam-ch <147068398+gautam-ch@users.noreply.github.com>
2025-02-02 11:50:48 +0530

    bootstrap the turborepo and configure tailwindcss

---
+++ paytm_turbo/apps/user_app/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@repo/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+                <h1 className="text-xl font-bold  underline">
+                  Hello world!asdsadasdasd
+                </h1>
+
+                <Button className="bg-blue-500 rounded-md text-xl p-3 " appName="ght">CLICK ME</Button>
+
+         
+    </div>
+  )
+}   
