809aaba888d3aef26b3b9e2e307123ff1228bb06
gautam-ch <147068398+gautam-ch@users.noreply.github.com>
2025-02-26 11:15:51 +0530

    add page to see recent transactions of user

--- paytm_turbo/apps/user_app/app/(dashboard)/transactions/page.tsx
+++ paytm_turbo/apps/user_app/app/(dashboard)/transactions/page.tsx
@@ -1,10 +1,39 @@
+import { getServerSession } from "next-auth"
+import { authOptions } from "../../lib/auth"
+
+import db from '@repo/db/client'
+import Transaction from "../../../components/Transactions";
 
 
 
-export default function Transactions(){
+async function getP2PTransfer(){
+    const session = await getServerSession(authOptions);
+      const tranx=await db.p2pTransfer.findMany({
+        where:{
+            OR:[
+             {fromUserId:Number(session.user.id)},
+             {toUserId:Number(session.user.id)}
+            ]
+        }
+      })
+
+      return {tranx,id:session.user.id};
+}
+
+
+
+
+
+
+
+export default async  function Transactions(){
+    const {tranx,id}=  await getP2PTransfer();
+    console.log('p2p transfer ',tranx,id);
     return (
-        <div>
-            Transactions page
+        <div className="w-full h-full">  
+              
+               <Transaction p2ptransfer={tranx} id={Number(id)}></Transaction>           
         </div>
     )
 }
+
---
+++ paytm_turbo/apps/user_app/components/Transactions.tsx
@@ -0,0 +1,80 @@
+import { Card } from "@repo/ui/card"
+
+
+interface p2pType{
+    id:number,
+    amount:number,
+    timestamp:Date,
+    fromUserId:number,
+    toUserId:number,
+    
+}
+
+interface  p2pTransfer{
+     p2ptransfer:p2pType[],
+     id:number
+}
+
+
+export default async function Transaction({p2ptransfer,id}:p2pTransfer){
+     
+            
+
+
+       return ( 
+              <div className="w-full h-full">
+                    <div className="text-3xl text-[#6a51a6] font-bold  mt-8 ml-3">
+                        Transactions
+                    </div>
+                   <div className="min-h-80 flex justify-center mt-10">
+                           
+                            <div className="w-[70%] shadow-xl rounded-xl p-2  border-t-2  ">
+                                <div className="flex justify-center">
+                                <div>
+                                        {
+                                        p2ptransfer.map(t=>(
+                                            t.fromUserId===id?Trnx('debit',t.amount,t.timestamp):Trnx('credit',t.amount,t.timestamp)
+                                        ))
+                                        }
+                                </div>
+                                </div>
+                            </div>
+                    </div>
+              </div>  
+       )
+}
+
+
+
+
+function Trnx(type:string,amount:number,time:Date){
+
+    return (
+           <div className="min-w-120 border-b-2  border-slate-400 m-2 flex justify-between">
+                 
+                 <div>
+                      
+                      <div className="text-1xl font-bold text-slate-600">
+                        {type}
+                      </div>
+                      <div className="text-xs text-slate-300 pt-1">
+                          {time.toDateString()}                         
+                      </div>
+                 </div>
+
+                 <div className=" flex items-center">
+                     
+                      <div className={`text-xl  ${type==='credit'?'text-green-500':'text-red-500'}`}>
+                               {`${type==='credit'?`+ ${amount/100} INR`:`-  ${amount/100} INR`}`}
+                      </div>
+                          
+
+                 </div>
+                  
+             
+                
+
+           </div>
+    )
+
+}
